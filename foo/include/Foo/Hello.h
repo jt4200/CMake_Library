734a5098dca32d6c87c0c9860dfb17be763f5527
jt4200 <jake@marshcroft.co.uk>
2020-01-22 09:36:25 +0000

    Intro

---
+++ foo/include/Foo/Hello.h
@@ -0,0 +1,7 @@
+#ifndef HELLO_H
+#define HELLO_H
+#include "foo_export.h"
+
+void FOO_EXPORT hello();
+
+#endif
---
+++ foo/src/Foo/Hello.cpp
@@ -0,0 +1,6 @@
+#include "foo/hello.h"
+#include <iostream>
+
+void hello(){
+	 std::cout << "Hello World" << std::endl;
+}
